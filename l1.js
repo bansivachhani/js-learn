e124683aa19b8b105dbd54a60779dd63594391c7
Bansi Vachhani <121160924+bansivachhani@users.noreply.github.com>
2024-12-28 21:45:02 +0530

    Add files via upload

---
+++ l1.js
@@ -0,0 +1,68 @@
+/*console.log("Bansi")
+
+//let const or var variables
+
+const accountId = 14435
+let accountEmail = "bansivachhani153@gmail.com"
+var accountPasss = "21360"
+accountCity = 'Jaipur'
+
+console.log(accountId);
+//accountId = 2 //not allowed //const are not redclared the value
+
+accountEmail = 'bansi@gmail.com'
+console.log(accountEmail);
+
+accountPasss = '2121'
+console.log(accountPasss);
+
+accountCity = 'Bengaluru'
+console.log(accountCity)
+/*prefer not to use var because of the scope so recently now is used 
+in your code is let and const variables.*/
+/*
+console.table([accountId,accountEmail,accountPasss,accountCity])
+
+*/
+
+//datatypes and ECMA
+"use strict"; //treat all js code as newer version
+
+console.log(3/3)
+
+console.log('hello')
+
+let name = 'bansi'
+let age = 19
+let isLoggedIn = true
+
+// number => 2 to power 53
+// BigInt
+// string => ""
+// Boolean => true/false
+// null => standalone value
+// undefined =>
+// Symbol => unique
+
+
+//object
+
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof isLoggedIn)
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
+
+
+//typeconversion
+let score = 18
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+let is = 1
+let val = Boolean(is)
+console.log(val)
